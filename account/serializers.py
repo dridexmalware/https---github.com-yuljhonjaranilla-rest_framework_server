47eaebf4235048a95456cec74ed412af32d42859
YUL JHON JARANILLA <yuljhonjaranilla@gmail.com>
2024-04-20 23:34:42 +0800

    Apr20-v3

--- account/serializers.py
+++ account/serializers.py
@@ -3,5 +3,9 @@ from account.models import User
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        model=User,
-        fields=('id', 'first_name', 'last_name', 'username', 'email', 'password', 'confirm_password')
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
